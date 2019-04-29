0e14e8977f6d374564d1ceec6a460af646196bfe
kennythepark <parkubc@gmail.com>
2019-04-29 16:37:41 -0700

    Updates

---
+++ precache-manifest.1c2866d214d1da1599d1608c783a93f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9472638af60f0d6696a6caaa72975fa",
+    "url": "/kennythepark/sdn1w-website/index.html"
+  },
+  {
+    "revision": "d1c5765f459ffd4d4dbc",
+    "url": "/kennythepark/sdn1w-website/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/kennythepark/sdn1w-website/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "d1c5765f459ffd4d4dbc",
+    "url": "/kennythepark/sdn1w-website/static/js/main.78b27484.chunk.js"
+  },
+  {
+    "revision": "0773c5d3d48cc3061448",
+    "url": "/kennythepark/sdn1w-website/static/js/runtime~main.33b7c48b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kennythepark/sdn1w-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
